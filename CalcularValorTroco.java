25ee182ee5a9fa5a0488ec7f479663718a6d3728
Anderson Cleiton <135235923+AndersonCldev@users.noreply.github.com>
2024-02-26 12:59:31 -0300

    Update CalcularValorTroco.java

--- CalcularValorTroco.java
+++ CalcularValorTroco.java
@@ -3,11 +3,11 @@ public class CalcularValorTroco {
 	public static void main(String[]args) {
 		
 // O  Programa que ler o valor de um produto e valor pago pelo cliente, faz o cálculo do valor de troco e mostra na tela.
-		Double valorDigitadoCliente, resultado;
+		Double valorDigitadoCliente, valorProduto, resultado;
 		Scanner Ler1 = new Scanner(System.in);
 		
 		System.out.print("Olá, digite o valor do produto: ");
-		Double valorProduto = Ler1.nextDouble();
+		valorProduto = Ler1.nextDouble();
 		
 		System.out.print("Digite quantidade passada pelo clietne:  ");
 		Double valorDigitadoCliente = Ler1.nextDouble();
